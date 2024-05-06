9b3c82e7f2c4c7d73574f64d9eeea47b505a1c47
zhyeonn <156568917+zhyeonn@users.noreply.github.com>
2024-05-06 18:55:37 +0900

    Update boardContentsItem.js

--- board/src/boardContentsItem.js
+++ board/src/boardContentsItem.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Board.css";
 
 const BoardContentsItem = (props) => {
+  const [comment, setComment] = useState("");
+  const [commentValid, setCommentValid] = useState("");
+  const [notAllow, setNotAllow] = useState(true);
+  const [commentList, setCommentList] = useState([]);
+
+  const handleComment = (e) => {
+    const commentInputValue = e.target.value;
+    setComment(commentInputValue);
+
+    const commentRegex = /^.{2,}$/;
+    setCommentValid(commentRegex.test(commentInputValue));
+  };
+
+  const handleAddComment = () => {
+    if (commentValid) {
+      setCommentList([...commentList, comment]);
+      setComment("");
+      setNotAllow(true);
+    }
+  };
+
+  useEffect(() => {
+    setNotAllow(!commentValid);
+  }, [commentValid]);
+
   return (
     <div>
       <table>
         <thead>
           <tr>
-            <td>제목</td>
-            <td>name</td>
-            <td>2024. 03. 16</td>
+            <td className="boardTdTitle btTitle">제목</td>
+            <td className="boardTdWriter btWriter">name</td>
+            <td className="boardTdDate btDate">2024. 03. 16</td>
           </tr>
         </thead>
         <tbody>
@@ -16,9 +42,30 @@ const BoardContentsItem = (props) => {
             <td colSpan="3">~~~</td>
           </tr>
           <tr>
-            <td>첨부파일</td>
-            <td colSpan="2">pdf</td>
+            <td colSpan="3">
+              댓글
+              <input
+                type="text"
+                className="boardCommentInput"
+                value={comment}
+                onChange={handleComment}
+              />
+              <button
+                disabled={notAllow}
+                onChange={handleComment}
+                className="boardCommentButton"
+                onClick={handleAddComment}
+              >
+                등록
+              </button>
+            </td>
           </tr>
+          {commentList.map((comm, index) => (
+            <tr key={index}>
+              <td>이름</td>
+              <td colSpan="2">{comm}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
